505e5ac21d099ba7cd7bbfad8b50c8e1d292b5d7
Ezaka Alfred <ezakaphil@gmail.com>
2020-11-16 05:35:11 +0100

    Add poppins font and css resets

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Let's do this
+      </header>
+    </div>
+  );
+}
+
+export default App;
